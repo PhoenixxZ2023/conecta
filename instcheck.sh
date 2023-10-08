4d2600148539708e12a9d21de1fce8d16fdb75ca
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2023-10-08 20:02:09 -0300

    Update instcheck.sh

--- instcheck.sh
+++ instcheck.sh
@@ -16,15 +16,15 @@ pip3 install flask > /dev/null 2>&1
 rm /bin/chuser > /dev/null 2>&1
 sleep 5
 cd /bin || exit
-wget https://raw.githubusercontent.com/PhoenixxZ2023/CheckUSER-4G/main/chuser > /dev/null 2>&1
-wget https://raw.githubusercontent.com/PhoenixxZ2023/CheckUSER-4G/main/userscheck > /dev/null 2>&1
+wget https://raw.githubusercontent.com/PhoenixxZ2023/conecta/main/c4g > /dev/null 2>&1
+wget https://raw.githubusercontent.com/PhoenixxZ2023/conecta/main/c4g > /dev/null 2>&1
 chmod 777 chuser > /dev/null 2>&1
 chmod 777 userscheck > /dev/null 2>&1
 clear
 mkdir /usr/lib/checkuser > /dev/null 2>&1
 cd /usr/lib/checkuser || exit
 rm checkuser.py > /dev/null 2>&1
-wget https://raw.githubusercontent.com/PhoenixxZ2023/CheckUSER-4G/main/checkuser.py > /dev/null 2>&1
+wget https://raw.githubusercontent.com/PhoenixxZ2023/conecta/main/c4g.py > /dev/null 2>&1
 chmod 777 checkuser.py > /dev/null 2>&1
 clear
 echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
